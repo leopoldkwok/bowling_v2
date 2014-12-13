4a7a1a8face371c4d50add4ea9156883da97ed45
leopoldkwok <leopoldkwok@gmail.com>
2014-12-13 12:03:28 +0000

    refactored creating new game

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,20 +2,20 @@ require 'bowling_game'
 
 describe 'Bowling Game' do 
 
-	it 'can create game' do
-		game = BowlingGame.new
+	before do
+		@game = BowlingGame.new
 	end
 
 	it 'can roll gutter game' do
 		game = BowlingGame.new
-		20.times {game.roll 0}
-		expect(game.score).to eq 0
+		20.times {@game.roll 0}
+		expect(@game.score).to eq 0
 	end
 
 	it 'can roll all ones' do
 		game = BowlingGame.new
-		20.times {game.roll 1}
-		expect(game.score).to eq 20
+		20.times {@game.roll 1}
+		expect(@game.score).to eq 20
 	end
 
 
