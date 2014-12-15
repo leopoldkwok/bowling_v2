005e63f1536595d8aee5f473457d11773423becd
leopoldkwok <leopoldkwok@gmail.com>
2014-12-15 14:53:00 +0000

    created test for can roll a perfect game

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -31,4 +31,9 @@ describe 'Bowling Game' do
 		16.times{@game.roll 0}
 		expect(@game.score).to eq 24
 	end
+
+	it 'can roll a perfect game' do
+		12.times {@game.roll 10}
+		expect(@game.score).to eq 300
+	end
 end
